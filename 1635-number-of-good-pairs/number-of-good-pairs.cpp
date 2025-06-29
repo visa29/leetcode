2a930f949f3f35812b72c980a3d060b075c87a68
visa29 <152060849+visa29@users.noreply.github.com>
2025-06-29 12:03:32 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (5.06%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
         unordered_map<int,int>m; //SC O(n)
+        int count=0;
         for(auto i:nums)
-        m[i]=m[i]+1;
-        int count = 0;
-        for(auto i:m)
         {
-            int k=i.second;
-            count = count + ((k*(k-1))/2);
+        count = count + m[i];
+        m[i]=m[i]+1;
         }
     return count;
 }
