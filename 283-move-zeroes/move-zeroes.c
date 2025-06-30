5c9d3505c31fd8827399d39f064f4a0b33da19c8
visa29 <152060849+visa29@users.noreply.github.com>
2025-06-30 15:05:36 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (46.42%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.c
@@ -0,0 +1,11 @@
+void moveZeroes(int* nums, int n) {
+    int j = 0;
+    for(int i = 0; i < n; i++) {
+        if(nums[i] != 0) {
+            nums[j++] = nums[i];
+        }
+    }
+    while(j < n) {
+        nums[j++] = 0;
+    }
+}
