f138d649b02396a09b684cfc3471b5e60e420c44
visa29 <152060849+visa29@users.noreply.github.com>
2025-07-04 12:07:59 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (49.01%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.c
+++ 1013-fibonacci-number/fibonacci-number.c
@@ -1,4 +1,7 @@
-int fib(int n){
-if(n==0||n==1) return n;
-return fib(n-2)+fib(n-1);
+int fib(int n)
+{
+    int f[31]={0,1};
+    for(int i=2;i<=n;i++)
+    f[i]=f[i-1]+f[i-2];
+    return f[n];
 }
